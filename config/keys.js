1cf5384517112b58249e252071adbe1507dcd993
Mridul Parakh <mridulparakh41@gmail.com>
2019-05-03 18:08:59 +0530

    finish

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports={
-    googleClientID:'998929040979-kk85nsov6381v00fgrm1149lubr1n26b.apps.googleusercontent.com',
-    googleClientSecret:'4LDoRLw3Tb683pjzqggwYSji',
-    mongoURI:'mongodb://review:review1@ds245228.mlab.com:45228/review',
-    key:"beareddddddddddddddddddddddddddddddr"
+if(process.env.NODE_ENV==='production'){
+    module.exports=require('./prod');
+}else{
+    module.exports=require('./dev');
 }
